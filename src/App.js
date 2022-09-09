8572a2cb9762acfb0e6081f06170c85310fe590f
ariandto <92358210+ariandto@users.noreply.github.com>
2022-09-09 11:56:44 +0700

    commitproject1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import Tasks from './Tasks';
+import {Paper,TextField} from '@material-ui/core';
+import {Checkbox,Button} from '@material-ui/core';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Tasks {
+  state = {tasks: [], currentTasks:""}
+  render() {
+    const {tasks} = this.state;
+    return(
+    <div className='App flex'>
+      <Paper elevation={3} className="container" >
+        <div className="heading">TO-DO-LIST APP</div>
+        <form
+        onSubmit={this.handleSubmit}
+        className="flex"
+        style={{margin: "15px"}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <TextField
+          variant="outlined"
+          size="small"
+          style={{width:"150px"}}
+          value={this.state.currentTask}
+          required={true}
+          onChange={this.handleChange}
+          placeholder="Add New To-Do"
+          />
+          <Button
+          style={{height:"40px"}}
+          color="primary"
+          variant="outlined"
+          type="submit"
+          >
+            Add task
+          </Button>
+        </form>
+        <div>
+          {tasks.map((task) => (
+            <Paper key={task._id} className="flex task_container">
+              <Checkbox
+                checked = {task.completed}
+                onClick={() => this.handleUpdate(task._id)}
+                color="primary"
+              />
+              <div
+              className={task.completed ?"task line_through" : "task"}
+              >
+                {task.task}
+              </div>
+              <Button
+              onClick={() => this.handleDelete(task._id)}
+              color="secondary"
+              >
+                delete
+              </Button>
+            </Paper>
+          ))}
+        </div>
+      </Paper>
+    </div>);
+  }
 }
 
 export default App;
